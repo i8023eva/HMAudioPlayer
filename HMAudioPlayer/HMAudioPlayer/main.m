7dbe6a1902eca550650f8ff40622c060fe9640dc
i8023eva <1009175863@qq.com>
2015-10-27 10:29:41 +0800

    初始化项目

---
+++ HMAudioPlayer/HMAudioPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HMAudioPlayer
+//
+//  Created by lyh on 15/10/27.
+//  Copyright © 2015年 lyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
